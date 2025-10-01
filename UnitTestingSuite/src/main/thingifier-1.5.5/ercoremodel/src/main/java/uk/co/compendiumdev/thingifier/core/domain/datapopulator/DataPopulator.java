7ff7521741c31c8841cf9f87e8a922f7a93b7508
ClemLoukie <146157452+ClemLoukie@users.noreply.github.com>
2025-10-01 10:30:14 -0400

    Mocha + Chai skeleton

--- UnitTestingSuite/src/main/thingifier-1.5.5/ercoremodel/src/main/java/uk/co/compendiumdev/thingifier/core/domain/datapopulator/DataPopulator.java
+++
@@ -1,8 +0,0 @@
-package uk.co.compendiumdev.thingifier.core.domain.datapopulator;
-
-import uk.co.compendiumdev.thingifier.core.EntityRelModel;
-
-public interface DataPopulator {
-
-    void populate(EntityRelModel model);
-}
